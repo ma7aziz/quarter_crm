c060a4a89dfe7ae1a3209a3770882d64c3c4c722
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-03-31 19:08:13 +0200

    create user accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
-
-
+from .models import User
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html', {'form': RepairRequestForm})
+
+
+def create_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        name = request.POST['name']
+        phone = request.POST['phone']
+        role = request.POST['role']
+        # file = request.FILES['attach_file']
+        if request.POST['password1'] == request.POST['password2']:
+            user = User(username=username, name=name,
+                        phone=phone, role=role)
+            user.set_password(request.POST['password1'])
+            user.save()
+            messages.success(request, "تم انشاء حساب المستخدم")
+        else:
+            messages.error(request,
+                           "كلمة السر غير متطابقة .. برجا المحاولة مرة اخري ")
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def userLogin(request):
--- quarter_crm/urls.py
+++ quarter_crm/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from accounts.views import userLogin as login
+from accounts.views import create_user as signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login', login, name='user_login'),
+    path('create_user', signup, name="create_user"),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('core.urls')),
     path('', include('service.urls')),
