22828b2f24ee71866fc55bd818a225c198d99f2b
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-05-20 15:36:25 +0200

    user views

--- core/views.py
+++ core/views.py
@@ -15,19 +15,19 @@ from .models import Customer
 
 
 def index(request):
+    # new_requests = Service_request.objects.new()
+    # under_process = Service_request.objects.under_process()
+    # done = Service_request.objects.done()
 
-    new_requests = Service_request.objects.new()
-    under_process = Service_request.objects.under_process()
-    done = Service_request.objects.done()
+    # tecnicians = User.objects.all().filter(role=3)
+    # ctx = {
 
-    tecnicians = User.objects.all().filter(role=3)
-    ctx = {
+    #     'new_request': new_requests,
+    #     'under_process': under_process,
+    #     'done': done,
+    #     'technicians': tecnicians}
 
-        'new_request': new_requests,
-        'under_process': under_process,
-        'done': done,
-        'technicians': tecnicians}
-    return render(request, 'index.html', ctx)
+    return render(request, 'index.html')
 
 
 def dashboard(request):
