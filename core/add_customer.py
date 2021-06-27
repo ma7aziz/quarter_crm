d4c325268ed2692a6a6101b20284cd2e12aa29b2
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2021-06-27 21:10:25 +0200

    fix customer name bug

--- core/add_customer.py
+++ core/add_customer.py
@@ -2,12 +2,9 @@ from .models import Customer
 
 
 def add_customer(phone, name, *email):
-    customer = Customer.objects.all().filter(phone=phone).first()
-    if not customer:
-        customer = Customer(name=name, phone=phone)
+    customer = Customer(name=name, phone=phone)
+    customer.save()
+    if email:
+        customer.email = email
         customer.save()
-        if email:
-            customer.email = email
-            customer.save()
-    print(customer)
     return customer
