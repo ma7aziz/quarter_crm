b4c60674c8598b3e5a20a71411b47c3ba3a32ceb
ma7aziz <45315298+ma7aziz@users.noreply.github.com>
2023-04-07 15:16:39 +0200

    settings

--- quarter_crm/settings.py
+++ quarter_crm/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'quarter_crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'crm',
-#         'USER': 'crm',
-#         'PASSWORD': 'crm123',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+#  }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm',
+        'USER': 'crm',
+        'PASSWORD': 'crm123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
